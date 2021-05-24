cb5190fc06440d6bb8c8a1f5f0855ae68704d63d
Jinny Lee <84109962+leejineui@users.noreply.github.com>
2021-05-24 21:15:24 +0900

    Update team99.py

--- team99.py
+++ team99.py
@@ -1,3 +1,32 @@
+import random
+count = 7
+sampleList = ['a', 'b', 'c', 'd', 'e']
+
+final= [random.choice(sampleList) for i in range(count)]
+print(final)
+
+final_no_du= random.sample(sampleList, count)
+print(final_no_du)
+
+
+
+
+
+
+import random
+count = 7
+sampleList = ['a', 'b', 'c', 'd', 'e']
+final= [random.choice(sampleList) for i in range(count)]
+print(final)
+final_no_du= random.sample(sampleList, count)
+print(final_no_du)
+
+
+
+
+
+
+
 '''
 Team 8 finish
 '''
@@ -8,4 +37,4 @@ final= [random.choice(sampleList) for i in range(COUNT)]
 print(final)
 final_no_du= random.sample(sampleList, COUNT)
 print(final_no_du)
-print("안녕")
+
