90c1920846bff02c97701f0e88311c84dd68d64d
Jinny <eui0326@gmail.com>
2021-05-24 20:57:49 +0900

    first commit

---
+++ team99.py
@@ -0,0 +1,10 @@
+'''
+Team 8 finish
+'''
+import random
+COUNT = 7
+sampleList = ['a', 'b', 'c', 'd', 'e']
+final= [random.choice(sampleList) for i in range(COUNT)]
+print(final)
+final_no_du= random.sample(sampleList, COUNT)
+print(final_no_du)
